62f57a2b83761eac1ee83d497b1b97cc39d16c7a
0megq <nvatanshenas55@gmail.com>
2023-10-24 09:37:50 -0700

    Fixed tile selection to work with sides of tiles

--- Mushroom Market/scenes/TileChecker.gd
+++ Mushroom Market/scenes/TileChecker.gd
@@ -24,29 +24,38 @@ func _process(delta: float) -> void:
 
 func place_tile_at_mouse(index: int, layer_list: Array[TileMap]) -> void:
 	var layer := layer_list[index]
+	if layer.get_cell_source_id(0, _current_tile) == -1:
+		layer.set_cell(0, _current_tile, 3, Vector2i(0, 0))
+		return
+	
 	if index + 1 >= layer_list.size():
 		return
 	var layer_above := layer_list[index + 1]
-	var tile_pos := layer.local_to_map(layer.get_local_mouse_position())
-	
-	layer_above.set_cell(0, tile_pos, 3, Vector2i(0, 0))
+	layer_above.set_cell(0, _current_tile, 3, Vector2i(0, 0))
 	
 	
 func delete_tile_at_mouse(index: int, layer_list: Array[TileMap]) -> void:
 	var layer := layer_list[index]
-	var tile_pos := layer.local_to_map(layer.get_local_mouse_position())
 	
-	layer.set_cell(0, tile_pos, -1)
+	layer.set_cell(0, _current_tile, -1)
 
 
 func _check_for_tiles(layer_list: Array[TileMap], tile_size: int) -> int:
 	for i in layer_list.size():
 		var layer: TileMap = layer_list[i]
+		
 		var tile_pos := layer.local_to_map(layer.get_local_mouse_position())
-		if layer.get_cell_source_id(0, tile_pos) != -1:
-			var offset := Vector2i.UP * (tile_size / 2 * (layer_list.size() - 1 - i))
-			$Polygon2D.global_position = tile_pos * tile_size + offset
-			return layer_list.size() - 1 - i
+		var offset_tile_pos := layer.local_to_map(layer.get_local_mouse_position() + Vector2.UP * tile_size / 2)
+		if layer.get_cell_source_id(0, tile_pos) != -1 or i == layer_list.size() - 1:
+			_current_tile = tile_pos
+		elif layer.get_cell_source_id(0, offset_tile_pos) != -1:
+			_current_tile = offset_tile_pos
+		else:
+			continue
+		
+		var offset := Vector2i.UP * (tile_size / 2 * (layer_list.size() - i - 1))
+		$Polygon2D.global_position = _current_tile * tile_size + offset
+		return layer_list.size() - 1 - i
 	
 	# add selection for blank tiles here
-	return -1
+	return 0
