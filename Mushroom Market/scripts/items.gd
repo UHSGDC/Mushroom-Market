7883abf0375be55e1f2a657a01c1435b017d2434
0megq <nvatanshenas55@gmail.com>
2023-11-02 09:43:45 -0700

    Functions for item and tile categorization

--- Mushroom Market/scripts/items.gd
+++ Mushroom Market/scripts/items.gd
@@ -1,13 +1,14 @@
 extends Node
 
 enum Use {
-	REPLACE_WHOLE,
-	PLACE_WHOLE,
-	USE,
+	TILEMAP,
+	PRODUCT,
+	OTHER
 }
 
 
 enum ID {
+	NO_ITEM = -1,
 	CAULDRON = 10, # 10 - 19 is reserved for crafting items
 	COMPOSTER,
 	FERMENTER,
@@ -28,13 +29,66 @@ var item_data: Array[ItemData] = [
 	preload("res://items/composter.tres"),
 ]
 
+var data_from_id: Dictionary
+var id_from_tile: Dictionary = { -1 : ID.NO_ITEM }
+
+func _ready() -> void:
+	initiliaze_item_dictionaries()
+
+
+func initiliaze_item_dictionaries() -> void:
+	for item in item_data:
+		if item.use_tags.has(Use.TILEMAP):
+			if id_from_tile.has(item.tile_id):
+				assert(false, "two items with same tile id")
+			id_from_tile[item.tile_id] = item.id
+		if data_from_id.has(item.id):
+			assert(false, "two items with same id")
+		data_from_id[item.id] = item
+
+
 func get_item_data(id: ID) -> ItemData:
 	if id < 0:
 		return null
 	
-	for item in item_data:
-		if item.id == id:
-			return item
+	var data: ItemData = data_from_id.get(id)
 	
-	assert(false, "ItemData for id: " + str(id) + " not found")
-	return null
+	if !data:
+		push_warning("ItemData for id: " + str(id) + " not found")
+
+	return data
+
+
+func get_id_from_tile(tile_id: int) -> ID:
+	var id: ID = id_from_tile.get(tile_id)
+	
+	if !id:
+		push_warning("ID for tile id: " + str(tile_id) + " not found")
+
+	return id
+
+
+func get_data_from_tile(tile_id: int) -> ItemData:
+	return get_item_data(get_id_from_tile(tile_id))
+
+
+func is_crafter(id: ID) -> bool:
+	return id >= 10 and id < 20
+	
+func is_light(id: ID) -> bool:
+	return id >= 20 and id < 30
+	
+func is_whole(id: ID) -> bool:
+	return id >= 40 and id < 70
+	
+func is_path(id: ID) -> bool:
+	return id >= 50 and id < 60
+	
+func is_soil(id: ID) -> bool:
+	return id >= 60 and id < 70
+	
+func is_dirt(id: ID) -> bool:
+	return id == ID.DIRT
+	
+func is_mushroom(id: ID) -> bool:
+	return id >= 70 and id < 90
