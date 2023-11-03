e79126631b3b874b304262f7f4258efa6af3870c
0megq <nvatanshenas55@gmail.com>
2023-11-02 20:06:56 -0700

    Tile placement and modes working
    fingers crossed the tilemap should be mostly implemented now. There are still some changes I want to make to the outline and possibly for invalid tiles.

--- Mushroom Market/scripts/tile_manager.gd
+++ Mushroom Market/scripts/tile_manager.gd
@@ -25,7 +25,6 @@ var mode: Mode = Mode.SELECT
 @onready var _tile_size: int = layers[0].tile_set.tile_size.x
 
 
-
 func _ready() -> void:
 	print(layers.size())
 	Global.item_selected.connect(_on_item_selected)
@@ -33,42 +32,42 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	_layer_index = _check_for_tiles(layers.duplicate(), _tile_size)
-	if Input.is_action_just_pressed("change_tile"):
+		
+		
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("change_tile"):
 		edit_tile_at_mouse(_layer_index, layers.duplicate())
 
 
 func _on_item_selected(item: Items.ID) -> void:
+	$Preview.texture = null
+	mode = Mode.SELECT
 	tile = Items.get_item_data(item)
 	if !tile:
-		mode = Mode.SELECT
 		return
-	$Preview.texture = tile.place_texture
+		
+	print(tile.name)
 	if tile.use_tags.has(Items.Use.TILEMAP):
+		$Preview.texture = tile.place_texture
 		mode = tile.place_mode
-	else:
-		mode = Mode.NONE
 
 
 func edit_tile_at_mouse(layer_index: int, layer_list: Array[TileMap]) -> void:
-	print("this needs to be implemeented again")
-
-
-#func edit_tile_at_mouse(layer_index: int, layer_list: Array[TileMap]) -> void:
-#	var layer := layer_list[layer_index]
-#	if layer.get_cell_source_id(0, _current_tile) == -1:
-#		layer.set_cell(0, _current_tile, 3, Vector2i(0, 0))
-#		return
-#
-#	if layer_index + 1 >= layer_list.size():
-#		return
-#	var layer_above := layer_list[layer_index + 1]
-#	layer_above.set_cell(0, _current_tile, 3, Vector2i(0, 0))
-	
-	
-func delete_tile_at_mouse(layer_index: int, layer_list: Array[TileMap]) -> void:
-	var layer := layer_list[layer_index]
-	
-	layer.set_cell(0, _current_tile, -1)
+	var layer = layer_list[layer_index]
+	if !_is_tile_valid:
+		return
+	match mode:
+		Mode.REMOVE:
+			layer.set_cell(0, _current_tile, -1)
+		Mode.SELECT:
+			print("selecting tile")
+		Mode.NONE:
+			print("currently doing nothing")
+		Mode.MUSHROOM_MODIFIER:
+			print("modify this shroom!!!")
+		_:
+			layer.set_cell(0, _current_tile, tile.tile_id, Vector2i(0, 0))
+			Global.change_inventory_item.call(tile.id, -1)
 	
 	
 func _check_for_tiles(layer_list: Array[TileMap], tile_size: int) -> int:
@@ -77,70 +76,68 @@ func _check_for_tiles(layer_list: Array[TileMap], tile_size: int) -> int:
 		var tile_pos := layer.local_to_map(layer.get_local_mouse_position())
 		var tile_id := layer.get_cell_source_id(0, tile_pos)
 		var tile_item_id: Items.ID = Items.get_id_from_tile(tile_id)
-		var tile_above_id := layer_list[i + 1].get_cell_source_id(0, tile_pos) if i < layer_list.size() - 1 else -1
+		var tile_above_id := layer_list[i + 1].get_cell_source_id(0, tile_pos) if i < layer_list.size() - 1 else -2
+		
+		var offset_tile_pos := layer.local_to_map(layer.get_local_mouse_position() + Vector2.UP * tile_size / 2)
+		var offset_tile_id :=  layer.get_cell_source_id(0, offset_tile_pos)
 		match mode:
 			Mode.MUSHROOM:
 				if tile_id != -1 and tile_above_id == -1 and Items.is_soil(tile_item_id):
 					_current_tile = tile_pos
-					$Preview.show()
 					i += 1
-					print("open!!!")
 				else:
-					$Preview.hide()
 					continue
-
 			Mode.OTHER_TILE:
 				if tile_id != -1 and tile_above_id == -1 and Items.is_whole(tile_item_id):
 					_current_tile = tile_pos
-					$Preview.show()
 					i += 1
-					print("open!!!")
 				else:
-					$Preview.hide()
 					continue
 			Mode.DIRT:
 				if tile_id != -1 and tile_above_id == -1:
 					_current_tile = tile_pos
-					$Preview.show()
 					i += 1
-					print("open!!!")
 				else:
-					$Preview.hide()
 					continue
 			Mode.SOIL:
-				pass
+				if Items.is_dirt(tile_item_id) and tile_above_id == -1:
+					_current_tile = tile_pos
+				else:
+					continue
 			Mode.PATH:
-				pass
+				if Items.is_dirt(tile_item_id) and tile_above_id == -1 and !Items.is_mushroom(Items.get_id_from_tile(tile_above_id)):
+					_current_tile = tile_pos
+				else:
+					continue
 			Mode.MUSHROOM_MODIFIER:
-				pass
+				if Items.is_mushroom(tile_item_id):
+					_current_tile = tile_pos
+				elif Items.is_mushroom(Items.get_id_from_tile(offset_tile_id)):
+					_current_tile = offset_tile_pos
+				else:
+					continue
 			Mode.SELECT:
-				pass
+				if tile_id != -1:
+					_current_tile = tile_pos
+				elif layer.get_cell_source_id(0, offset_tile_pos) != -1:
+					_current_tile = offset_tile_pos
+				else:
+					continue
 			Mode.REMOVE:
-				pass
+				if tile_id != -1:
+					_current_tile = tile_pos
+				elif layer.get_cell_source_id(0, offset_tile_pos) != -1:
+					_current_tile = offset_tile_pos
+				else:
+					continue
 			Mode.NONE:
 				print("mode is none lmao lol rofl xD")
 		var offset := Vector2i.UP * (tile_size / 2 * i)
+		$Preview.show()
+		_is_tile_valid = true
 		$Preview.global_position = _current_tile * tile_size + offset
 		return i
-
-	print("invalid")
-	return 0
-
-
-#func _check_for_tiles(layer_list: Array[TileMap], tile_size: int) -> int:
-#	for i in range(layer_list.size() - 1, -1, -1): # need to make match statements z... z.. for each mode z.. z.. z... ... 
-#		var layer: TileMap = layer_list[i]
-#
-#		var tile_pos := layer.local_to_map(layer.get_local_mouse_position())
-#		if layer.get_cell_source_id(0, tile_pos) != -1:
-#			_current_tile = tile_pos
-#		elif i == 0:
-#			_current_tile = tile_pos
-#		else:
-#			continue
-#
-#		var offset := Vector2i.UP * (tile_size / 2 * i)
-#		$Preview.global_position = _current_tile * tile_size + offset
-#		return i
-#
-#	return 0
+	
+	$Preview.hide()
+	_is_tile_valid = false
+	return -1
